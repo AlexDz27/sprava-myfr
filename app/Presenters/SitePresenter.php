b547e8c0a04bba8fc3848ee53f785ca1ac0bec4a
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-07-10 12:49:16 +0300

    w routing

--- app/Presenters/SitePresenter.php
+++ app/Presenters/SitePresenter.php
@@ -5,6 +5,5 @@ require 'BasePresenter.php';
 class SitePresenter extends BasePresenter {
   public function simplePage($pathToPage, $title = 'Some title') {
     require $pathToPage;
-    die();
   }
 }
--- front-end/parts/head.php
+++ front-end/parts/head.php
@@ -6,5 +6,5 @@
   <meta name="robots" content="noindex,nofollow,noarchive">
   <link rel="stylesheet" href="./front-end/assets/css/main.h-2.css">
   <script src="./front-end/assets/js/main.h-2.js" defer></script>
-  <title>TODO: title properly</title>
+  <title><?= $title ?></title>
 </head>
