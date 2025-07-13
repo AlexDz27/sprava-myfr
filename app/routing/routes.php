405a635a857e0e7f2dda13be4e1ed3ac621a95e8
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-07-13 15:37:26 +0300

    w

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -22,6 +22,9 @@ return [
   ]],
   '404' => [$sitePresenter, 'simplePage', [
     'title' => $sitePresenter->companyName. ' | Страница не найдена',
+    'extraAssets' => [
+      '<script src="https://api-maps.yandex.ru/v3/?apikey=2b7d9147-4a30-4c29-9a2f-594b90fa8c59&lang=ru_RU" defer></script>',
+    ],
     'pathToPage' => 'front-end/pages/404.php'
   ]]
 ];
