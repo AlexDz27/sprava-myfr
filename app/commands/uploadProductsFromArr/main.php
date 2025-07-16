c6de54f5bb8c6d5a2213ebbac38993107e39c9c6
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-07-16 18:23:27 +0300

    w, fin products table

--- app/commands/uploadProductsFromArr/main.php
+++ app/commands/uploadProductsFromArr/main.php
@@ -5,24 +5,29 @@ $pdo = require 'app/Data/pdo.php';
 
 $products = $db['products']['sprava'];
 
-$arts = ['0100-0000-10', '0100-0000-15', '0100-0000-20'];
-for ($i = 0; $i <= 2; $i++) {
-  $product = $products[$arts[$i]];
-  $pdo->exec("INSERT INTO products VALUES (
-    null,
-    '{$product['art']}',
-    '{$product['price']}',
-    '{$product['category_id']}',
-    '{$product['model']}',
-    '{$product['variant']}',
-    '{$product['unit']}',
-    '{$product['upakMal']}',
-    '{$product['upakKrup']}',
-    '{$product['description']}',
-    0,
-    '{$product['img']}',
-    null,
-    '{$product['excelFileRowNum']}',
-    null
-  )");
+foreach ($products as $product) {
+  // $pdo->exec("INSERT INTO products VALUES (
+  //   null,
+  //   '{$product['art']}',
+  //   '{$product['price']}',
+  //   '{$product['category_id']}',
+  //   '{$product['model']}',
+  //   '{$product['variant']}',
+  //   '{$product['unit']}',
+  //   '{$product['upakMal']}',
+  //   '{$product['upakKrup']}',
+  //   '{$product['description']}',
+  //   0,
+  //   '{$product['img']}',
+  //   null,
+  //   '{$product['excelFileRowNum']}',
+  //   null
+  // )");
+
+  $glued = glue($product);
+  $pdo->exec("UPDATE products SET name = '$glued' WHERE art = '{$product['art']}'");
+}
+
+function glue($p) {
+  return $p['model'] . ', ' . $p['variant'];
 }
