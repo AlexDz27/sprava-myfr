a633441ddda0349bb32188e4ea9cdfa9346348dc
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-18 13:46:51 +0300

    w

--- front-end/site/parts/head.php
+++ front-end/site/parts/head.php
@@ -3,16 +3,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="noindex,nofollow,noarchive">
 
   <?php if ($pageName == 'about-company'): ?>
     <meta name="description" content="О нас | SPRAVA | Оптовые поставки строительных материалов">
+    <meta property="og:site_name" content="Компания SPRAVA">
+    <meta property="og:title" content="О нас | SPRAVA | Оптовые поставки строительных материалов">
   <?php elseif ($pageName == 'catalog'): ?>
     <meta name="description" content="Каталог | SPRAVA | Оптовые поставки строительных материалов">
+    <meta property="og:site_name" content="Компания SPRAVA">
+    <meta property="og:title" content="Каталог | SPRAVA | product_descr">
   <?php elseif ($pageName == 'table'): ?>
     <meta name="description" content="Прайс-лист | SPRAVA | Оптовые поставки строительных материалов">
+    <meta property="og:site_name" content="Компания SPRAVA">
+    <meta property="og:title" content="Прайс-лист | SPRAVA | Оптовые поставки строительных материалов">
   <?php else: ?>
     <meta name="description" content="SPRAVA | Оптовые поставки строительных материалов">
+    <meta property="og:site_name" content="Компания SPRAVA">
+    <meta property="og:title" content="SPRAVA | Оптовые поставки строительных материалов">
   <?php endif ?>
 
   <link rel="stylesheet" href="/front-end/site/assets/css/style.h-1.css">
